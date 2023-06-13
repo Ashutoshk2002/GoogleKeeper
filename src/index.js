ebcfa5e242c3dd6f0d965ba8b6f7fef8b6555ee5
Ashutoshk2002 <90946675+Ashutoshk2002@users.noreply.github.com>
2023-06-13 21:44:24 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from './App';
-// import Header from "./Header";
+import App from "./App";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import card from "./Cards";
 
 ReactDom.render(
   <div>
+    <App />
+    {/* <Note /> */}
 
-    <App/>
-    <Note />
+
+    {/* {card.map((card) => {
+      return <Note 
+      key={card.id} 
+      title={card.title}  
+      content={card.content} />;
+    })} */}
+
+
     <Footer />
   </div>,
   document.getElementById("root")
